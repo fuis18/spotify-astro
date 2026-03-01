c1004131dfce07de7baef5cf58a6533b94802469
fuis18 <warfuis18larcz018@gmail.com>
2026-02-28 21:04:40 -0500

    adapter to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import svelte from '@astrojs/svelte';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +12,6 @@ export default defineConfig({
   },
 
   output: 'server',
-  integrations: [svelte(), react()]
+  integrations: [svelte(), react()],
+  adapter: vercel(),
 });
